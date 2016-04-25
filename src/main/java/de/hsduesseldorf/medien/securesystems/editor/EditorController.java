a07e26b5c07e613fe4bfb2ccb415057bb6b769f2
Hendrik Brinkmann <hendrik@brinkmann.one>
2016-04-25 23:31:44 +0200

    add simple event handling

--- src/main/java/de/hsduesseldorf/medien/securesystems/editor/EditorController.java
+++ src/main/java/de/hsduesseldorf/medien/securesystems/editor/EditorController.java
@@ -1,5 +1,34 @@
 package de.hsduesseldorf.medien.securesystems.editor;
 
-public class EditorController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class EditorController implements Initializable {
+
+    @FXML
+    Button helloButton;
+
+    @FXML
+    Button clearButton;
+
+    @FXML
+    Label label;
+
+    @FXML
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        this.helloButton.setOnAction((event)->{
+            label.setText("Hello World!");
+        });
+
+        this.clearButton.setOnAction((event -> {
+            label.setText("");
+        }));
+    }
 }

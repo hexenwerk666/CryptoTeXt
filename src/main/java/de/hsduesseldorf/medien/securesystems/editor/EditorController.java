225bd5e12aff3e84f35c8d9eba1ab1f28656ffe6
Hendrik Brinkmann <hendrik@brinkmann.one>
2016-04-26 11:01:41 +0200

    add simple editor functionality

--- src/main/java/de/hsduesseldorf/medien/securesystems/editor/EditorController.java
+++ src/main/java/de/hsduesseldorf/medien/securesystems/editor/EditorController.java
@@ -1,33 +1,72 @@
 package de.hsduesseldorf.medien.securesystems.editor;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextArea;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.net.URL;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class EditorController implements Initializable {
 
+    public static final String STATIC_FILE_PATH = "/tmp/editorTest";
     @FXML
-    Button helloButton;
+    MenuItem menuFileSave;
 
     @FXML
-    Button clearButton;
+    MenuItem menuFileLoad;
 
     @FXML
-    Label label;
+    MenuItem menuFileQuit;
+
+    @FXML
+    TextArea text;
 
     @FXML
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        this.helloButton.setOnAction((event)->{
-            label.setText("Hello World!");
-        });
+        menuFileSave.setOnAction((event) -> save());
+        menuFileLoad.setOnAction((event) -> load());
+        menuFileQuit.setOnAction((event) -> quit());
+    }
 
-        this.clearButton.setOnAction((event -> {
-            label.setText("");
-        }));
+    private boolean save() {
+        byte[] data = text.getText().getBytes();
+        Path file = Paths.get(STATIC_FILE_PATH);
+        try {
+            Files.write(file, data);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    private boolean load() {
+        Path file = Paths.get(STATIC_FILE_PATH);
+        try {
+            byte[] data = Files.readAllBytes(file);
+            text.setText(new String(data));
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+
+    }
+
+    private void quit() {
+        Platform.exit();
+        System.exit(0);
     }
 }
